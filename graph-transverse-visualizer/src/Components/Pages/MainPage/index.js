a73c2693cd860df72636452c14654dcd7c840040
sberdine77 <sbsx77xavier@hotmail.com>
2021-03-10 22:34:16 -0300

    Step by step algorith vizualization working

--- graph-transverse-visualizer/src/Components/Pages/MainPage/index.js
+++ graph-transverse-visualizer/src/Components/Pages/MainPage/index.js
@@ -2,213 +2,139 @@ import React, {useState, useEffect} from 'react';
 import {Graph} from 'react-d3-graph';
 import Grafo from '../../../Models/Grafo';
 
-function MainPage() {
+class MainPage extends React.Component {
 	// graph payload (with minimalist structure)
 
-	
-	//const [nodes, setNodes] = useState([]);
-	//const [links, setLinks] = useState([]);
+	constructor(props) {
+		super(props);
+		this.graph = new Grafo();
+		this.graph.addNode(1);
+		this.graph.addNode(2);
+		this.graph.addNode(3);
+		this.graph.addNode(4);
+		this.graph.addNode(5);
+		this.graph.addNode(6);
+		this.graph.addNode(7);
+		this.graph.addNode(8);
+		this.graph.addNode(9);
+		this.graph.addNode(10);
 
-	const [data, setData] = useState({});
+		this.graph.addLink(1, 2);
+		this.graph.addLink(1, 3);
+		this.graph.addLink(2, 4);
+		this.graph.addLink(2, 5);
+		this.graph.addLink(3, 6);
+		this.graph.addLink(3, 7);
+		this.graph.addLink(4, 8);
+		this.graph.addLink(5, 9);
+		this.graph.addLink(8, 9);
+		this.graph.addLink(9, 10);
+		this.graph.addLink(6, 10);
 
-	var graph = new Grafo();
-	graph.addNode(1);
-	graph.addNode(2);
-	graph.addNode(3);
-	graph.addNode(4);
-	graph.addNode(5);
-	graph.addNode(6);
-	graph.addNode(7);
-	graph.addNode(8);
-	graph.addNode(9);
-	graph.addNode(10);
+		const nodesTemp = [];
+		const linksTemp = [];
 
-	graph.addLink(1, 2);
-	graph.addLink(1, 3);
-	graph.addLink(2, 4);
-	graph.addLink(2, 5);
-	graph.addLink(3, 6);
-	graph.addLink(3, 7);
-	graph.addLink(4, 8);
-	graph.addLink(5, 9);
-	graph.addLink(8, 9);
-	graph.addLink(9, 10);
-	graph.addLink(6, 10);
-
-	useEffect(() => {
-		async function load() {
-			const nodesTemp = [];
-			const linksTemp = [];
-
-			for(var key in graph.adjacencyList) {
-				if(graph.adjacencyList.hasOwnProperty(key)){
-					nodesTemp.push({ id: key })
-					graph.adjacencyList[key].forEach(child => {
-						linksTemp.push({ source: `${key}`, target: `${child}` })
-					})
-				}
+		for(var key in this.graph.adjacencyList) {
+			if(this.graph.adjacencyList.hasOwnProperty(key)){
+				nodesTemp.push({ id: key })
+				this.graph.adjacencyList[key].forEach(child => {
+					linksTemp.push({ source: `${key}`, target: `${child}` })
+				})
 			}
-
-			// await setNodes(nodesTemp);
-			// await setLinks(linksTemp);
-
-			setData({
-				nodes: nodesTemp,
-				links: linksTemp
-			});
-		}
-		load();
-	}, []);
-
-	// the graph configuration, just override the ones you need
-	const myConfig = {
-		nodeHighlightBehavior: true,
-		directed: true,
-		node: {
-		color: "lightgreen",
-		size: 120,
-		highlightStrokeColor: "blue",
-		},
-		link: {
-		highlightColor: "lightblue",
-		},
-	};
-
-	function sleep(milliseconds) {
-		const date = Date.now();
-		let currentDate = null;
-		do {
-		  currentDate = Date.now();
-		} while (currentDate - date < milliseconds);
-	}
-	
-	const onClickNode = function(nodeId) {
-		window.alert(`Clicked node ${nodeId}`);
-	};
-	
-	const onClickLink = function(source, target) {
-		window.alert(`Clicked link between ${source} and ${target}`);
-	};
-	
-	const onClickDFS = async () => {
-		const path = graph.dfs(1);
-		console.log(path);
-		var tempNodes = data.nodes;
-		for(var i in path) {
-			for (var j in data.nodes) {
-				console.log(data.nodes[j])
-				console.log("------")
-				if(parseInt(data.nodes[j].id) === path[i]) {
-					tempNodes[j] = {
-						...data.nodes[j],
-						color: "red"
-					}
-				} else {
-					tempNodes[j] = data.nodes[j]
-				}
-			}
-			// tempNodes.push(data.nodes.map(node => {
-			// 	if((parseInt(node.id)) === path[i]) {
-			// 		console.log((parseInt(node.id)))
-			// 		console.log(path[i])
-			// 		tempNodes[i] = {
-			// 			...node,
-			// 			color: "red"
-			// 		}
-			// 		return {
-			// 			...node,
-			// 			color: "red"
-			// 		}
-			// 	} else {
-			// 		tempNodes[i] = node
-			// 		return node
-			// 	}
-			// }))
-
-			//await setNodes(tempNodes)
-
-			// setNodes((prev) => ({...prev, nodes: tempNodes}))
-
-			// setData((prev) => ({...prev, nodes: tempNodes}))
-
-			// await setData((prev) => ({...prev, nodes: prev.nodes.map(object => {
-			// 	if(path[i] === (parseInt(object.id))) {
-			// 		console.log("red")
-			// 		return {
-			// 			...object,
-			// 			color: "red"
-			// 		}
-			// 	} else {
-			// 		return object;
-			// 	}
-			// }) }));
 		}
 
-		setData(
-			((prev) => ({...prev, nodes: data.nodes})),
-			() => {
-				const context = {
-					tempNodes,
-					nodes: data.nodes
-				}
-				runRecursively(context.nodes, context)
-			}
-		);
-		
+		let data = {
+			nodes: nodesTemp,
+			links: linksTemp
+		}
+
+		this.state = {
+			data: data
+		}
+
 	}
 
-	const runRecursively = (newNodes, context) => {
-		const { processing } = context.tempNodes
-		setData((prev) => ({...prev, nodes: newNodes}), async () => {
-			const item = processing.shift()
-			context.nodes = await delay(item, context)
-			if(processing.length > 0) {
-				runRecursively(context.nodes, context);
-			} else {
-				finalizeProcess(context)
-			}
-		})
-	}
+	changeColor = async function(nodeIds) {
+		let modData = { ...this.state.data };
+		let nodeId = nodeIds.shift()
+		let selectNode = modData.nodes.filter(item => {
+		  return item.id === `${nodeId}`;
+		});
+		selectNode.forEach(item => {
+		  if (item.color && item.color === "red") item.color = "blue";
+		  else item.color = "red";
+		});
+		await this.longTask()
+		//testDelayedState(reactRef, modData)
+		this.setState({ data: modData }, () => {
+			this.changeColor(nodeIds)
+		});
+	};
 
-	const delay = async (item, context) => {
+	longTask = async () => {
 		return new Promise((resolve) => {
-			context.nodes.map(node => {
-				if(node.id === item.id) {
-					return {
-						...node,
-						color: "red"
-					}
-				} else {
-					return node
-				}
-			})
-			setTimeout(() => {
-			  resolve(context.nodes);
-			}, 3000);
+		  setTimeout(() => {
+			resolve();
+		  }, 500);
 		});
 	}
 
-	const finalizeProcess = (context) => {
-		setData((prev) => ({...prev, nodes: context.nodes}))
-	}
+	render() {
 
-	const onClickBFS = () => {
+		// the graph configuration, just override the ones you need
+		const myConfig = {
+			nodeHighlightBehavior: true,
+			directed: true,
+			node: {
+			color: "lightgreen",
+			size: 120,
+			highlightStrokeColor: "blue",
+			},
+			link: {
+			highlightColor: "lightblue",
+			},
+		};
 		
-	}
+		const onClickNode = (nodeId) => {
+			window.alert(`Clicked node ${nodeId}`);
+		};
+		
+		const onClickLink = function(source, target) {
+			window.alert(`Clicked link between ${source} and ${target}`);
+		};
+		
+		const onClickDFS = () => {
 
-	return(
-		<>
-			<button onClick={onClickDFS}>DFS</button>
-			<button onClick={onClickBFS}>BFS</button>
-			<Graph
-				id="graph-id" // id is mandatory
-				data={data}
-				config={myConfig}
-				onClickNode={onClickNode}
-				onClickLink={onClickLink}
-			/>
-		</>
-	);
+			console.log("DFS")
+
+			const path = this.graph.dfs(1);
+
+			this.changeColor(path)
+			
+		}
+
+		const onClickBFS = () => {
+			console.log("DFS")
+
+			const path = this.graph.bfs(1);
+
+			this.changeColor(path)
+		}
+
+		return(
+			<>
+				<button onClick={onClickDFS}>DFS</button>
+				<button onClick={onClickBFS}>BFS</button>
+				<Graph
+					id="graph-id" // id is mandatory
+					data={this.state.data}
+					config={myConfig}
+					onClickNode={onClickNode}
+					onClickLink={onClickLink}
+				/>
+			</>
+		)
+	}
 }
 
 export default MainPage;
